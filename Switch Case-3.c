33dbf5af7cb5ae3d57ea7d8f1637238224cbeca9
TashrinTanisha <145369121+TashrinTanisha@users.noreply.github.com>
2024-01-11 18:42:48 +0200

    Switch Case-3 is completed

---
+++ Switch Case-3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a = 2;
+
+    switch(a)
+    {
+    case 1 :
+
+        printf("The 1st case works\n");
+        break;
+
+    case 2 :
+
+        printf("The 2nd case works\n");
+        break;
+
+    case 3 :
+
+        printf("The 3rd case works\n");
+        break;
+
+    default :
+
+        printf("Nothing works\n");
+    }
+
+    return 0;
+}
